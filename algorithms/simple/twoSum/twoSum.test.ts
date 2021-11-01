82e8734be5b289ffda87c577f78977073b27b80e
HadHod <maciej.mlynarski@gmail.com>
2021-11-01 09:11:10 +0100

    added types to two sum

--- algorithms/simple/twoSum/twoSum.test.ts
+++ algorithms/simple/twoSum/twoSum.test.ts
@@ -1,6 +1,10 @@
 import { twoSum } from './twoSum';
 
 describe('twoSum', () => {
+  test('should return [-1, -1]', () => {
+    expect(twoSum([1, 100], 2)).toStrictEqual([-1, -1]);
+  });
+
   test('should return [3, 5]', () => {
     expect(twoSum([1, 3, 5, 2, 6, 8, 9], 10)).toStrictEqual([3, 5]);
   });
--- algorithms/simple/twoSum/twoSum.ts
+++ algorithms/simple/twoSum/twoSum.ts
@@ -1,5 +1,5 @@
-export function twoSum(numbers: number[], sum: number): any {
-  const previousValues: any = {};
+export function twoSum(numbers: number[], sum: number): [number, number] {
+  const previousValues: { [key: number]: number } = {};
   for (let i=0; i<numbers.length; i++) {
     const neededValue: number = sum - numbers[i];
     const index: number = previousValues[neededValue];
@@ -10,4 +10,6 @@ export function twoSum(numbers: number[], sum: number): any {
 
     previousValues[numbers[i]] = i;
   }
+
+  return [-1, -1];
 }
