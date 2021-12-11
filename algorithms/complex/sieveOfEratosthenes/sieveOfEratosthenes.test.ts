436881a20bee425249d459c8146fd475bdc675e1
HadHod <maciej.mlynarski@gmail.com>
2021-12-11 22:05:45 +0100

    unit tests to sieveOfEratosthenes

--- algorithms/complex/sieveOfEratosthenes/sieveOfEratosthenes.test.ts
+++ algorithms/complex/sieveOfEratosthenes/sieveOfEratosthenes.test.ts
@@ -1,7 +1,13 @@
 import { sieveOfEratosthenes } from './sieveOfEratosthenes';
 
 xdescribe('sieveOfEratosthenes', () => {
-  test('should return X', () => {
-    expect(sieveOfEratosthenes(3)).toBe([1,2,1]);
+  test('should return one prime number', () => {
+    expect(sieveOfEratosthenes(1)).toBe([2]);
+  });
+
+  test('should return 10 prime numbers', () => {
+    const primes: number[] = sieveOfEratosthenes(10);
+    expect(primes.length).toBe(10);
+    expect(primes).toBe([2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
   });
 });
