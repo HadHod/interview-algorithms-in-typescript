1047763e5e5ab85d2ab26ae5af689bb23fc028ea
HadHod <maciej.mlynarski@gmail.com>
2021-12-21 16:58:33 +0100

    depthFirstSearch first version added

--- algorithms/complex/depthFirstSearch/depthFirstSearch.ts
+++ algorithms/complex/depthFirstSearch/depthFirstSearch.ts
@@ -1,3 +1,15 @@
-export function depthFirstSearch(word: string): string {
-  throw new Error('Not implemented');
+export class Tree<T> {
+  constructor(
+    public value: T,
+    public left?: Tree<T>,
+    public right?: Tree<T>,
+  ) {}
+}
+
+export function depthFirstSearch<T>(callback: any, node?: Tree<T>): void {
+  if (node !== undefined) {
+    depthFirstSearch(callback, node.left);
+    callback(node.value);
+    depthFirstSearch(callback, node.right);
+  }
 }
